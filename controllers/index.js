c38673e95e2f0b2bc72d8395bf0a5df8abc5fb9f
Roy4th <97925568+Roy4th@users.noreply.github.com>
2022-07-29 12:41:08 -0500

    cleaned code

--- controllers/index.js
+++ controllers/index.js
@@ -1,15 +1,15 @@
-const router = require('express').Router()
+const router = require("express").Router();
 
-const apiRouters = require('./api');
-const homeRoutes = require('./homepage-routes');
-const dashboardRoutes = require('./dashboard-routes');
+const apiRouters = require("./api");
+const homeRoutes = require("./homepage-routes");
+const dashboardRoutes = require("./dashboard-routes");
 
-router.use('/api', apiRouters);
-router.use('/', homeRoutes);
-router.use('/dashboard', dashboardRoutes)
+router.use("/api", apiRouters);
+router.use("/", homeRoutes);
+router.use("/dashboard", dashboardRoutes);
 
 router.use((req, res) => {
-    res.status(404).end();
+  res.status(404).end();
 });
 
 module.exports = router;
--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,26 +1,28 @@
 async function editFormHandler(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const title = document.querySelector('input[name="post-title"]').value.trim();
-    const id = window.location.toString().split('/')[
-        window.location.toString().split('/').length - 1
-    ];
-    console.log(id)
-    const response = await fetch(`/api/post/${id}`, {
-        method: 'PUT',
-        body: JSON.stringify({
-            title
-        }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
+  const title = document.querySelector('input[name="post-title"]').value.trim();
+  const id = window.location.toString().split("/")[
+    window.location.toString().split("/").length - 1
+  ];
+  console.log(id);
+  const response = await fetch(`/api/post/${id}`, {
+    method: "PUT",
+    body: JSON.stringify({
+      title,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 
-    if (response.ok) {
-        document.location.replace('/dashboard/');
-    } else {
-        alert(response.statusText);
-    }
+  if (response.ok) {
+    document.location.replace("/dashboard/");
+  } else {
+    alert(response.statusText);
+  }
 }
 
-document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
+document
+  .querySelector(".edit-post-form")
+  .addEventListener("submit", editFormHandler);
