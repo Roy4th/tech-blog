de81a8f1eefc682a425a5eda84f0561190973217
Roy4th <97925568+Roy4th@users.noreply.github.com>
2022-07-29 12:45:07 -0500

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# tech-blog
+# Tech-Blog
+  ![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+  # Description
+  This application is a personal blog that I may edit or add to whenever I need to. 
+  * [Description](#discription)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contribution](#contribution)
+  * [Questions](#questions)
+  
+  # Installation
+  First get all the files on to your local machine by downloading it or cloning it. once you have the files go ahead in your terminal and run -npm i to download all the files you will need to run the application. After that is finished in your terminal run -npm start to run the application.
+  # License
+  MIT
+Copyright (c) 2022
+      
+for more information about MIT License click [link](https://opensource.org/licenses/MIT)
+  # Contribution
+  If you would like to contribute to this project add to the issues tab on Github.
+  -Paul Soliz
+  # Questions
+  If you have any question you can contact here: 
+  [Roy4th](github.com/Roy4th)
+email: roy4th@yahoo.com
--- server.js
+++ server.js
@@ -1,42 +1,40 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
-const path = require('path');
-const session = require('express-session');
+const express = require("express");
+const exphbs = require("express-handlebars");
+const path = require("path");
+const session = require("express-session");
 
-const sequelize = require('./config/connection');
-const routes = require('./controllers');
-const helpers = require('./utils/helpers');
-
+const sequelize = require("./config/connection");
+const routes = require("./controllers");
+const helpers = require("./utils/helpers");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-const hbs = exphbs.create({ helpers })
-const sequelizeStore = require('connect-session-sequelize')(session.Store)
+const hbs = exphbs.create({ helpers });
+const sequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const sess = {
-    secret: 'Super secret secret',
-    cookie: {},
-    resave: false,
-    saveUninitialized: true,
-    store: new sequelizeStore({
-        db: sequelize
-    })
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new sequelizeStore({
+    db: sequelize,
+  }),
 };
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(session(sess));
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars')
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(routes);
 
-
 // sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => {
-        console.log(`APP INITIALIZED ON PORT ${PORT}!`)
-    })
-});   
+  app.listen(PORT, () => {
+    console.log(`APP INITIALIZED ON PORT ${PORT}!`);
+  });
+});
